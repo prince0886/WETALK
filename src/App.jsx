62dc1f76d1888ff5b3e054db7ff87e98e0ae1c6d
prince0886 <prince.kumar22b@iiitg.ac.in>
2025-09-25 22:48:34 +0530

    add your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import assets from './assets/assets' // Import assets
 
 const App = () => {
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')]
-    bg-conatin">
+    <div
+      style={{
+        backgroundImage: `url(${assets.bgImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        minHeight: '100vh'
+      }}
+    >
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/WETALK' element={<HomePage />} />  {/* Add this line */}
+        <Route path='/WETALK' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/profile' element={<ProfilePage />} />
       </Routes>
